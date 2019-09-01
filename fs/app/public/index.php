8f3cd58cc0c771006f22e70e3e8a78fdb4405cad
Supun Budhajeewa <budhajeewa@gmail.com>
2019-09-01 16:29:50 +0530

    Work!

---
+++ fs/app/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo '<h1>A2Way Docker-Base PHP_FPM-Laravel</h1>';
+
+echo '<ul>
+<li><a href="https://a2way.com/">a2way.com</a></li>
+<li><a href="https://github.com/a2way-com/docker-base-php-fpm-laravel">github.com/a2way-com/docker-base-php-fpm-laravel</a></li>
+<li><a href="https://hub.docker.com/r/a2way/docker-base-php-fpm-laravel">hub.docker.com/r/a2way/docker-base-php-fpm-laravel</a></li>
+</ul>';
